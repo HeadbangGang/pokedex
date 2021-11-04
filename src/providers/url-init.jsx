8ce8cdf10b1b42fd7c97a24981d0a44a69ea60a6
Tayden <tayden@Taydens-MBP.satx.rr.com>
2021-11-04 03:01:37 -0500

    fixing merge conflicts

--- src/providers/url-init.jsx
+++ src/providers/url-init.jsx
@@ -9,11 +9,7 @@ export const UrlInit = (props) => {
         setUrlInit(
             process.env.NODE_ENV === 'development'
                 ? 'http://localhost:3001'
-<<<<<<< HEAD
                 : 'https://desolate-basin-78066.herokuapp.com'
-=======
-                : 'https://desolate-basin-78066.herokuapp.com/'
->>>>>>> 6881bc1504177ca4e572814e86f877ad0ea41f90
         )
     }, [])
 
